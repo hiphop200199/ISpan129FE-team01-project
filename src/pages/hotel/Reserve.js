32c47a81ae866d05d290c0a03b677d461702d3b8
Jin22221 <117150611+Jin22221@users.noreply.github.com>
2023-02-15 16:39:17 +0800

    修改reserve.js

--- src/pages/hotel/Reserve.js
+++ src/pages/hotel/Reserve.js
@@ -1,4 +1,6 @@
+import React from 'react'
 import { useState } from 'react'
+import Calendar from '../../img/hotels/calendar-plus-regular.svg'
 
 function Reserve() {
   const [petCount, setPetCount] = useState(0)
@@ -6,48 +8,46 @@ function Reserve() {
   const [adultCount, setAdultCount] = useState(0)
   return (
     <>
-      <div className="banner">
-        <div className="rd-img-left"></div>
-        <div className="rd-img-right">
-          <div className="img img1"></div>
-          <div className="img img2"></div>
+      <div className="rd-wrap">
+        <div className="banner">
+          <div className="rd-img-left"></div>
+          <div className="rd-img-right">
+            <div className="img img1"></div>
+            <div className="img img2"></div>
 
-          <div className="img img3"></div>
-          <div className="img img4"></div>
-        </div>
-      </div>
-      <div className="rd-container">
-        <div className="rd-content-left">
-          <div className="title">
-            <h1>標準雙人房</h1>
-            <h2>1800</h2>
-          </div>
-          <p>床型尺寸：一張標準雙人床 </p>
-          <p>
-            客房設施：空調, 地毯, 隔音, 清潔用品, 吊衣架, 淋浴, 吹風機,
-            免費盥洗用品, 獨立洗手間及浴室, 書桌, 拖鞋, 衛生紙, 電話, 有線頻道,
-            平面電視, 冰箱, 電熱水壺, 毛巾, 床單。
-          </p>
-          <p>
-            所有客房以下服務免費提供：WiFi、晨間喚醒、夜間加枕/加被、備品補充。
-          </p>
-          <div className="notice">
-            <p>注意事項</p>
+            <div className="img img3"></div>
+            <div className="img img4"></div>
           </div>
         </div>
-        <div className="rd-content-right">
-          <div className="r-wrap">
+        <div className="rd-container">
+          <div className="rd-content-left">
+            <div className="title">
+              <h1>標準雙人房</h1>
+              <h2>1800</h2>
+            </div>
+            <p>床型尺寸：一張標準雙人床 </p>
+            <p>
+              客房設施：空調, 地毯, 隔音, 清潔用品, 吊衣架, 淋浴, 吹風機,
+              免費盥洗用品, 獨立洗手間及浴室, 書桌, 拖鞋, 衛生紙, 電話,
+              有線頻道, 平面電視, 冰箱, 電熱水壺, 毛巾, 床單。
+            </p>
+            <p>
+              所有客房以下服務免費提供：WiFi、晨間喚醒、夜間加枕/加被、備品補充。
+            </p>
+            <div className="notice">
+              <p>注意事項</p>
+            </div>
+          </div>
+          <div className="rd-content-right">
+            {/* <div className="r-wrap"> */}
             <div className="r-form">
               <div className="r-form-day">
-                <div className="chech-in">
-                  <img
-                    src={require('../../img/hotels/calendar-plus-regular.svg')}
-                    alt=""
-                  />
+                <div className="check-in">
+                  <img src={Calendar} alt="Calendar" width={30} height={30} />
                   <p>入住日期</p>
                 </div>
                 <div className="check-out">
-                  {/* <img src={require('../img/寵物床Amazon.com.jpg')} alt="" /> */}
+                  <img src={Calendar} alt="Calendar" width={30} height={30} />
                   <p>退房日期</p>
                 </div>
               </div>
@@ -117,7 +117,9 @@ function Reserve() {
               {petCount > 0 ? (
                 <div className="choose-pet-section">
                   <img src={require('../../img/hotels/cat-room1.png')} alt="" />
+                  <input type={'radio'}></input>
                   <img src={require('../../img/hotels/cat-room2.jpg')} alt="" />
+                  <input type={'radio'}></input>
                 </div>
               ) : (
                 ''
@@ -136,10 +138,20 @@ function Reserve() {
               </div>
             </div>
             <div className="r-btn-box">
-              <button class="r-btn r-btn-blue" type="button">
+              <button
+                type="button"
+                className="btn btn-outline-primary btn-lg min-width-auto mr-10px"
+              >
+                加入收藏
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary btn-lg min-width-auto ml-10px"
+              >
                 預訂
               </button>
             </div>
+            {/* </div> */}
           </div>
         </div>
       </div>
