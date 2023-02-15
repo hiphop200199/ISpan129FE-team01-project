67d3159302d8ce426d043fed5921177be4df74a1
Chu-Ya Hsu <t199511137@gmail.com>
2023-02-15 10:23:13 +0800

    首頁加上輪播套件

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,24 @@
 import blackDog from '../img/layout/camylla-battani-JgdgKvYgiwI-unsplash.jpg'
 import brownDog from '../img/layout/ayla-verschueren-bpkBLrotO28-unsplash.jpg'
+import Slider from 'react-slick'
+
+const settings = {
+  dots: true,
+  infinite: true,
+  slidesToShow: 1, // 一次顯示幾張
+  slidesToScroll: 1, // 按下一頁的時候，要跑幾張
+  autoplay: true,
+  speed: 3000,
+  autoplaySpeed: 5000,
+  cssEase: 'linear',
+}
 function HomePage() {
   return (
     <>
-      <img src={brownDog} alt="brownDog" />
-      <img src={blackDog} alt="blackDog" />
+      <Slider {...settings}>
+        <img src={brownDog} alt="brownDog" />
+        <img src={blackDog} alt="blackDog" />
+      </Slider>
     </>
   )
 }
