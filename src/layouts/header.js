dc048295436a6865c7a5e8d86fa8a5ea2a311123
Yun,Ning <dackdada016@gmail.com>
2023-02-16 12:36:28 +0800

    feat: 引入heder區塊功能

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -9,28 +9,20 @@ function Header() {
       <nav class="breadcrumb" aria-label="Breadcrumb">
         <ol class="breadcrumbList">
           <li>
-            <Link to="/">首頁</Link>
+            <Link to="/" className="Breadcrumb__link">
+              首頁
+            </Link>
           </li>
           <li>
-            <Link to="/">商城</Link>
+            <Link to="/Product" className="Breadcrumb__link">
+              商城
+            </Link>
           </li>
           <li class="current">
             <em aria-current="page">商品詳細</em>
           </li>
         </ol>
       </nav>
-      <div class="main-botton">
-        <Link to="/">罐頭飼料</Link>
-        <Link to="/">外出用品</Link>
-        <Link to="/">耐磨玩具</Link>
-        <Link to="/">服飾項圈</Link>
-        <form class="header-search">
-          <input type="search" placeholder="搜尋" name="" />
-          <button type="text">
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </button>
-        </form>
-      </div>
     </>
   )
 }
