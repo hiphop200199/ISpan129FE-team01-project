d00f9ebd21e0152bc4d63978750e66ecc9679c21
Yun,Ning <dackdada016@gmail.com>
2023-02-20 17:49:26 +0800

    feat: 繼承頁面的ID props

--- src/template/Card.js
+++ src/template/Card.js
@@ -1,22 +1,34 @@
 // import photo from '../img/productDetails/cheese.jpg'
-function Card({ photo }) {
-  // { photo }帶入參數
+// { photo }帶入參數
+// function Card({photo}) {
+
+import MoreSquare from './MoreSquare'
+
+function Card(props) {
+  const {
+    product_id,
+    product_name,
+    product_class,
+    product_price,
+    product_description,
+    product_unit,
+  } = props
   return (
     <>
       <div className="productCard">
         <section className="text-part">
-          <h2 className="title">潔牙零食</h2>
-          <p className="description">
-            問題的關鍵究竟為何？用最佳的策略去分析潔牙玩具。
-          </p>
-          <span className="price">NT. 300</span>
+          <h2 className="title">{product_name}</h2>
+          <p className="description">{product_description}</p>
+          <span className="price">NT. {product_price}</span>
         </section>
         <section className="buttons">
           <button className="button-collection">&#9825;</button>
           <button className="button-moreInfo">看更多</button>
+          <MoreSquare />
         </section>
         {/* 圖片動態引入 ，圖片須放在public資料夾*/}
-        <img src={'http://localhost:3000/' + photo} alt="" />
+        <img src={'http://localhost:3000/'} alt="" />
+        {/* <img src={'http://localhost:3000/' + photo} alt="" /> */}
         {/* <img src={photo} alt="" /> */}
       </div>
     </>
