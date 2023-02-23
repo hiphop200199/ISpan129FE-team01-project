8462444b8800275d04586ae67c0f72a3107bf113
Yun,Ning <dackdada016@gmail.com>
2023-02-23 15:30:41 +0800

    fix: 修正傳入參數錯誤導致無法取得id值

--- src/template/MoreSquare.js
+++ src/template/MoreSquare.js
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
-function MoreSquare(props) {
-  const { product_id } = props
+function MoreSquare({ product_id }) {
+  // const [id, setID] = useState('')
+  console.log(product_id)
   return (
     <>
       {/* 黃色小按鈕(圓弧形): 看更多，已結束，立即訂房 */}
       <button
         type="button"
         className="btn btn-secondary btn-sm min-width-auto "
+        // onClick={setID}
       >
         {/* <Link to={'/product/Detail/' + product_id}>看更多</Link> */}
         <Link to={`/product/Detail/${product_id}`}>看更多</Link>
