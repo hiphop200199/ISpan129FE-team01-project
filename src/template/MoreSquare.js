e6f238f075eb17c3d78de4c66f7ed5662c16c9c8
Yun,Ning <dackdada016@gmail.com>
2023-02-23 18:05:19 +0800

    feat: 新增共用元件用類別篩選URL的功能

--- src/template/MoreSquare.js
+++ src/template/MoreSquare.js
@@ -1,10 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
-function MoreSquare({ product_id }) {
+function MoreSquare({ product_id, type_id }) {
   // const [id, setID] = useState('')
-  console.log(product_id)
+  // type_id !== 1
+  //   ? `/product/Detail/${product_id}`
+  //   : `/hotel/Detail/${product_id}`
+  // console.log(type_id)
+  const [URL, setURL] = useState('')
+  // 利用type_id辨識共用元件應該導向哪個網址
+  useEffect(() => {
+    switch (type_id) {
+      case 1:
+        setURL(`/product/Detail/${product_id}`)
+        break
+      case 2:
+        setURL(`/course/${product_id}`)
+        break
+      case 3:
+        setURL(`/hotel/Detail/${product_id}`)
+        break
+      case 4:
+        setURL(`/meals/${product_id}`)
+        break
+      default:
+        setURL(`/product/Detail/${product_id}`)
+    }
+  }, [product_id, type_id])
+  // console.log(product_id)
   return (
     <>
       {/* 黃色小按鈕(圓弧形): 看更多，已結束，立即訂房 */}
@@ -14,7 +38,7 @@ function MoreSquare({ product_id }) {
         // onClick={setID}
       >
         {/* <Link to={'/product/Detail/' + product_id}>看更多</Link> */}
-        <Link to={`/product/Detail/${product_id}`}>看更多</Link>
+        <Link to={URL}>看更多</Link>
       </button>
     </>
   )
