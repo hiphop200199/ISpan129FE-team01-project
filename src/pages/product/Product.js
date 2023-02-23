82cf44ea97615e3a11b18db1e5f53b512da65a11
Yun,Ning <dackdada016@gmail.com>
2023-02-23 15:28:13 +0800

    fix: 修正API URL錯誤

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -10,24 +10,30 @@ function Product() {
   const { typeID } = useParams()
   const [data, setData] = useState([])
 
+  // 取得DB資料
   useEffect(() => {
     const fetchData = async () => {
       try {
+        console.log(typeID)
         const res = await fetch(
-          `http://localhost:3002/product/product/list-product/${typeID}`
+          `http://localhost:3002/product/list-product/${typeID}`
         )
+
+        // const rData = await res.json()
+        // console.log('rData', rData)
         if (!res.ok) {
           throw new Error('Network res was not ok')
         }
+
         const product = await res.json()
         setData(product)
+        console.log(data)
       } catch (err) {
         console.log(err)
       }
     }
     fetchData()
   }, [typeID])
-
   // 取得DB的資料(跨表取得img table的商品圖片)
   // useEffect(() => {
   //   fetch(`http://localhost:3002/product/list-product/${type}${search}`)
@@ -52,17 +58,12 @@ function Product() {
       <Header />
       <HeaderSearch />
       <h2>這裡之後放輪播器</h2>
-      <CardList data={data} />
+      <div className="content">
+        {data.map((product) => (
+          <Card key={product.id} data={product} />
+        ))}
+      </div>
     </>
-  )
-}
-function CardList({ data }) {
-  return (
-    <div className="content">
-      {data.map((product) => (
-        <Card key={product.id} data={product} />
-      ))}
-    </div>
   )
 }
 
