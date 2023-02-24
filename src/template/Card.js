c7e80ebde3eb80d5dbae6012a3477ed673819e69
Yun,Ning <dackdada016@gmail.com>
2023-02-24 14:34:48 +0800

    fix: 修改卡片顯示欄位缺少products_descripttion的問題

--- src/template/Card.js
+++ src/template/Card.js
@@ -12,28 +12,26 @@ function Card({ data }, { photo }) {
     type_id,
     product_class,
     products_price,
-    product_description,
+    products_descripttion,
   } = data
   return (
-    <>
-      <div className="productCard">
-        <span>{product_id}</span>
-        <section className="text-part">
-          <h2 className="title">{product_name}</h2>
-          <span>{product_class}</span>
-          <p className="description">{product_description}</p>
-          <span className="price">NT. {products_price}</span>
-        </section>
-        <section className="buttons">
-          <button className="button-collection">&#9825;</button>
-          <MoreSquare type_id={type_id} product_id={product_id} />
-        </section>
-        {/* 圖片動態引入 ，圖片須放在public資料夾*/}
-        <img src={`http://localhost:3000/${photo}`} alt="" />
-        {/* <img src={'http://localhost:3000/' + photo} alt="" /> */}
-        {/* <img src={photo} alt="" /> */}
-      </div>
-    </>
+    <div className="productCard">
+      <span hidden>{product_id}</span>
+      {/* <section className="text-part"> */}
+      <h2 className="title">{product_name}</h2>
+      <span hidden>{product_class}</span>
+      <p className="description">{products_descripttion}</p>
+      <span className="price">NT. {products_price}</span>
+      {/* </section> */}
+      <section className="buttons">
+        <button className="button-collection">&#9825;</button>
+        <MoreSquare type_id={type_id} product_id={product_id} />
+      </section>
+      {/* 圖片動態引入 ，圖片須放在public資料夾*/}
+      <img src={`http://localhost:3000/${photo}`} alt="" />
+      {/* <img src={'http://localhost:3000/' + photo} alt="" /> */}
+      {/* <img src={photo} alt="" /> */}
+    </div>
   )
 }
 export default Card
