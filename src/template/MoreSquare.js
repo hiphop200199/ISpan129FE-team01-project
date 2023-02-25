93776b919652b791bd86d22edfc156f41d51d1be
Yun,Ning <107627148+dackdada016@users.noreply.github.com>
2023-02-26 01:23:21 +0800

    fix: 修正switch無法正確跳轉的BUG

--- src/template/MoreSquare.js
+++ src/template/MoreSquare.js
@@ -1,44 +1,45 @@
 import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom'
-function MoreSquare({ product_id, type_id }) {
+import { Link, useNavigate } from 'react-router-dom'
+function MoreSquare({ product_id, typeID }) {
   // const [id, setID] = useState('')
   // type_id !== 1
   //   ? `/product/Detail/${product_id}`
   //   : `/hotel/Detail/${product_id}`
-  // console.log(type_id)
+  console.log(typeID)
+  console.log(product_id)
+  const navigate = useNavigate();
   const [URL, setURL] = useState('')
   // 利用type_id辨識共用元件應該導向哪個網址
+
+  const urlMap = {
+    1: `/product/Detail/${product_id}`,
+    2: `/course/${product_id}`,
+    3: `/hotel/Detail/${product_id}`,
+    4: `/meals/${product_id}`,
+  };
+
   useEffect(() => {
-    switch (type_id) {
-      case 1:
-        setURL(`/product/Detail/${product_id}`)
-        break
-      case 2:
-        setURL(`/course/${product_id}`)
-        break
-      case 3:
-        setURL(`/hotel/Detail/${product_id}`)
-        break
-      case 4:
-        setURL(`/meals/${product_id}`)
-        break
-      default:
-        setURL(`/product/Detail/${product_id}`)
-    }
-  }, [product_id, type_id])
+    setURL(urlMap[typeID] || '/');
+  }, [product_id, typeID]);
+
   // console.log(product_id)
+  const handleClick = () => {
+    navigate(URL);
+  };
+
   return (
     <>
       {/* 黃色小按鈕(圓弧形): 看更多，已結束，立即訂房 */}
       <button
         type="button"
         className="btn btn-secondary btn-sm min-width-auto "
-        // onClick={setID}
+        onClick={handleClick}
+      // onClick={setID}
       >
         {/* <Link to={'/product/Detail/' + product_id}>看更多</Link> */}
-        <Link to={URL}>看更多</Link>
+        <Link to={URL} product_id={product_id}>看更多</Link>
       </button>
     </>
   )
