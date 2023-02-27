c1ceb7d8046884b7dd3434b95eb795b33237c1ba
Jin22221 <117150611+Jin22221@users.noreply.github.com>
2023-02-27 15:13:30 +0800

    feat: 從session取reserve頁面資料,帶入reserveConfirm頁

--- src/pages/hotel/ReserveConfirm.js
+++ src/pages/hotel/ReserveConfirm.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { format } from 'date-fns'
 
 function ReserveConfirm() {
+  const [reserveConfirm, setReserveConfirm] = useState({
+    roomCount: 1,
+    petCount: 0,
+    childCount: 0,
+    adultCount: 1,
+    selectPet: '',
+    startDate: new Date(),
+    endDate: new Date().setDate(new Date().getDate() + 1),
+    differenceInDay: 1,
+    money: 0,
+    total: 0,
+  })
+
+  useEffect(() => {
+    setReserveConfirm(JSON.parse(sessionStorage.getItem('reserveData')))
+  }, [])
+  function formatDate(date) {
+    return date ? format(date, 'yyyy/MM/dd') : ''
+  }
+  const img = require(`../../img/hotels/${
+    reserveConfirm.selectPet === 'forest' ? 'cat-room1.png' : 'cat-room2.jpg'
+  }`)
   return (
     <>
-      <div className="rc-container">
-        <div className="rc-content-left">
+      <div className="rc-container rwd-container">
+        <div className="rc-content-left rwd-col-12">
           <div className="rc-text-header">確認並付款</div>
           <div className="rc-title">付款方式</div>
           <form>
@@ -86,50 +109,69 @@ function ReserveConfirm() {
             </div>
           </form>
         </div>
-        <div className="rc-content-right">
+        <div className="rc-content-right rwd-col-12">
           <div className="r-wrap">
             <div className="r-form">
               <div className="r-form-day">
                 <div className="check-in">
                   <p>入住日期</p>
+                  <p>{formatDate(new Date(reserveConfirm.startDate))}</p>
                 </div>
                 <div className="check-out">
                   <p>退房日期</p>
+                  <p>{formatDate(new Date(reserveConfirm.endDate))}</p>
+                </div>
+              </div>
+              <div className="r-form-adult">
+                <p>房間數量</p>
+                <div className="calculate-btn-box">
+                  <span>{reserveConfirm.roomCount}</span>
                 </div>
               </div>
               <div className="r-form-adult">
                 <p>成人</p>
                 <div className="calculate-btn-box">
-                  <span>1</span>
+                  <span>{reserveConfirm.adultCount}</span>
                 </div>
               </div>
               <div className="r-form-child">
                 <p>兒童</p>
                 <div className="calculate-btn-box">
-                  <span>1</span>
+                  <span>{reserveConfirm.childCount}</span>
                 </div>
               </div>
               <div className="r-form-pet">
                 <p>寵物</p>
                 <div className="calculate-btn-box">
-                  <span>1</span>
+                  <span>{reserveConfirm.petCount}</span>
                 </div>
               </div>
-              <p>您選擇的寵物窩造型:</p>
-              <div className="choose-pet-section">
-                <img src={require('../../img/hotels/cat-room1.png')} alt="" />
-              </div>
+              {reserveConfirm.selectPet ? (
+                <>
+                  <p>您選擇的寵物窩造型:</p>
+                  <div className="choose-pet-section">
+                    <img src={img} alt="" />
+                  </div>
+                </>
+              ) : (
+                ''
+              )}
             </div>
             <div className="r-price">
               <div className="price-day">
-                <p>$1800 TWD *1晚</p>
-                <p>$1800 TWD </p>
+                <p>
+                  ${reserveConfirm.money} TWD *{reserveConfirm.differenceInDay}
+                  晚
+                </p>
+                <p>${reserveConfirm.money} TWD </p>
               </div>
-
+              <div className="room-count">
+                <p>*{reserveConfirm.roomCount}間</p>
+              </div>
               <hr />
               <div className="total-price">
                 <p>總價</p>
-                <p>$2300 TWD</p>
+                <p>${reserveConfirm.total} TWD</p>
               </div>
             </div>
             <div className="r-btn-box">
