830d6aa43aa7777b99a3902380eb7d9ef3126e81
Eric Tsai <hiphop200199@gmail.com>
2023-03-01 11:16:23 +0800

    寫搜尋課程

--- src/pages/course/CourseSearch.js
+++ src/pages/course/CourseSearch.js
@@ -1,19 +1,42 @@
 import { Link } from 'react-router-dom'
 import dog from '../../img/course/dog.jpg'
-import child from '../../img/course/child.jpg'
-import corgi from '../../img/course/corgi.jpg'
-import dogAndBoy from '../../img/course/dog-and-boy.jpg'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 function CourseSearch() {
   const [courseName, setCourseName] = useState('')
   const [showCourses, setShowCourses] = useState([])
-  useEffect(() => {
-    axios.get('http://localhost:3002/courses').then((res) => {
+  const trainingCourses = () => {
+    axios.get(`http://localhost:3002/courses/寵物訓練`).then((res) => {
       const courses = res.data
       setShowCourses({ courses })
     })
-  }, [])
+  }
+  const interactiveCourses = () => {
+    axios.get(`http://localhost:3002/courses/寵物互動`).then((res) => {
+      const courses = res.data
+      setShowCourses({ courses })
+    })
+  }
+  const petKnowledges = () => {
+    axios.get(`http://localhost:3002/courses/寵物知識`).then((res) => {
+      const courses = res.data
+      setShowCourses({ courses })
+    })
+  }
+  const takeCarePets = () => {
+    axios.get(`http://localhost:3002/courses/寵物照顧`).then((res) => {
+      const courses = res.data
+      setShowCourses({ courses })
+    })
+  }
+  const findOneCourse = () => {
+    const userInput = courseName
+    axios.get(`http://localhost:3002/courses/${userInput}`).then((res) => {
+      const courses = res.data
+      setShowCourses({ courses })
+    })
+  }
+
   return (
     <>
       <div className="course-container">
@@ -27,20 +50,26 @@ function CourseSearch() {
               }}
               placeholder="搜尋"
             />
-            <button id="search-button">&#128269;</button>
+            <button id="search-button" onClick={findOneCourse}>
+              &#128269;
+            </button>
           </section>
-          <Link to="/courseSearch">
-            <button className="tag">寵物訓練</button>
-          </Link>
-          <Link to="/courseSearch">
-            <button className="tag">寵物互動</button>
-          </Link>
-          <Link to="/courseSearch">
-            <button className="tag">寵物知識</button>
-          </Link>
-          <Link to="/courseSearch">
-            <button className="tag">寵物照顧</button>
-          </Link>
+
+          <button className="tag" onClick={trainingCourses}>
+            寵物訓練
+          </button>
+
+          <button className="tag" onClick={interactiveCourses}>
+            寵物互動
+          </button>
+
+          <button className="tag" onClick={petKnowledges}>
+            寵物知識
+          </button>
+
+          <button className="tag" onClick={takeCarePets}>
+            寵物照顧
+          </button>
         </section>
 
         <section className="course-search-results">
@@ -68,7 +97,7 @@ function CourseSearch() {
             )
           })}
 
-          <article className="course-cards">
+          {/*<article className="course-cards">
             <div className="productCard">
               <section className="text-part">
                 <h2 className="title">寵物概論</h2>
@@ -84,74 +113,9 @@ function CourseSearch() {
                   <button className="button-moreInfo">看更多</button>
                 </Link>
               </section>
-              {/* 圖片動態引入 ，圖片須放在public資料夾*/}
-              <img src={dog} alt="" />
-              {/* <img src={photo} alt="" /> */}
+              
             </div>
-          </article>
-          <article className="course-cards">
-            <div className="productCard">
-              <section className="text-part">
-                <h2 className="title">寵物清潔</h2>
-                <span className="product-unit">32hrs</span>
-                <p className="description">
-                  讓專業獸醫師教您如何清潔寵物的口腔、毛髮等等。
-                </p>
-                <span className="price">NT. 600</span>
-              </section>
-              <section className="buttons">
-                <button className="button-collection">&#9825;</button>
-                <Link to="/courseDetail">
-                  <button className="button-moreInfo">看更多</button>
-                </Link>
-              </section>
-              {/* 圖片動態引入 ，圖片須放在public資料夾*/}
-              <img src={child} alt="" />
-              {/* <img src={photo} alt="" /> */}
-            </div>
-          </article>
-          <article className="course-cards">
-            <div className="productCard">
-              <section className="text-part">
-                <h2 className="title">寵物進階</h2>
-                <span className="product-unit">52hrs</span>
-                <p className="description">
-                  指導寵物的進階概念，對寵物有更深的認識。
-                </p>
-                <span className="price">NT. 700</span>
-              </section>
-              <section className="buttons">
-                <button className="button-collection">&#9825;</button>
-                <Link to="/courseDetail">
-                  <button className="button-moreInfo">看更多</button>
-                </Link>
-              </section>
-              {/* 圖片動態引入 ，圖片須放在public資料夾*/}
-              <img src={corgi} alt="" />
-              {/* <img src={photo} alt="" /> */}
-            </div>
-          </article>
-          <article className="course-cards">
-            <div className="productCard">
-              <section className="text-part">
-                <h2 className="title">人寵互動</h2>
-                <span className="product-unit">62hrs</span>
-                <p className="description">
-                  您了解您的寵物嗎?這堂課告訴您與寵物互動最重要的認知，減少不必要的煩惱!
-                </p>
-                <span className="price">NT. 900</span>
-              </section>
-              <section className="buttons">
-                <button className="button-collection">&#9825;</button>
-                <Link to="/courseDetail">
-                  <button className="button-moreInfo">看更多</button>
-                </Link>
-              </section>
-              {/* 圖片動態引入 ，圖片須放在public資料夾*/}
-              <img src={dogAndBoy} alt="" />
-              {/* <img src={photo} alt="" /> */}
-            </div>
-          </article>
+          </article>*/}
         </section>
       </div>
     </>
