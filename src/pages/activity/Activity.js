c677399696cde6838c05948ca5c0d4563fd8a175
4A555076 <fz555076@gmail.com>
2023-03-02 16:15:17 +0800

    feat : 新增活動卡片

--- src/pages/activity/Activity.js
+++ src/pages/activity/Activity.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import EventRegistration from '../../template/EventRegistration'
-import { Link } from 'react-router-dom'
+import { EventRegistration } from '../../template'
+import { Link, useParams } from 'react-router-dom'
+import SignUp from '../../template/SignUp'
+
+import Activityphoto from '../../img/activityphoto.json'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+
+import { Autoplay, Pagination, Navigation } from 'swiper'
 function Activity() {
   const [activity, setActivity] = useState([])
 
   useEffect(() => {
-    fetch(`http://localhost:3002/activity/api`, {})
+    fetch(`http://localhost:3002/activity/api`)
       .then((response) => response.json())
       .then((data) => {
         console.log('data', data)
@@ -19,10 +27,32 @@ function Activity() {
       {/* <Header />
       <HeaderSearch /> */}
 
+      <Swiper
+        style={{
+          '--swiper-navigation-color': 'white',
+          '--swiper-navigation-size': '20px',
+        }}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        slidesPerView={1}
+        modules={[Autoplay, Pagination, Navigation]}
+        loop={true}
+      >
+        {Activityphoto.map((photo, idx) => (
+          <SwiperSlide key={idx}>
+            <img src={photo.url} alt="" width="100%" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
       <div className="card-wrap">
-        {activity.map((el) => {
+        {activity.map((el, idx) => {
           return (
-            <div className="h-card col-6">
+            <div key={idx} className="h-card col-6">
               <div className="h-card-left col-6">
                 <div className="h-card-header">
                   <span hidden>{el.activity_id}</span>
@@ -33,17 +63,24 @@ function Activity() {
                   </p>
                   <p className="h-card-text">
                     截止日期:
-                    {new Date(el.activity_enddate).toString('yyyy-mm-dd')}
+                    {new Date(el.activity_dateend).toString('yyyy-MM-dd')}
                   </p>
                   <span hidden>{el.activity_pettype}</span>
                   <span hidden>{el.activity_location}</span>
                   <span hidden>{el.activity_decription}</span>
                   <span hidden>{el.activity_notice}</span>
                 </div>
-                <div className="h-card-footer">
-                  <Link to="/ActivityDetail">
-                    <EventRegistration />
-                  </Link>
+                <div className="row justify-content-around">
+                  <div className="col-6">
+                    <Link to={`/ActivitySignUp/${el.activity_id}`}>
+                      <SignUp />
+                    </Link>
+                  </div>
+                  <div className="col-6">
+                    <Link to={`/activitydetail/${el.activity_id}`}>
+                      <EventRegistration />
+                    </Link>
+                  </div>
                 </div>
               </div>
               <div className="h-card-right col-7">
