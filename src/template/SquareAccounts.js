5052018c5becfcd8d75f0919190c3e7b1a33315d
Yun,Ning <dackdada016@gmail.com>
2023-03-02 18:41:09 +0800

    feat: 新增共用元件

---
+++ src/template/SquareAccounts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SquareAccounts() {
+  return (
+    <button
+      type="button"
+      className="btn btn-primary btn-lg min-width-auto col-2"
+    >
+      結帳
+    </button>
+  )
+}
+
+export default SquareAccounts
