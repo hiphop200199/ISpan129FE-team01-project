b90eb6145a59380cce0fc1fca3f113f1562a84c7
Yun,Ning <dackdada016@gmail.com>
2023-03-03 15:03:31 +0800

    style: 調整button 樣式

--- src/template/SquareAccounts.js
+++ src/template/SquareAccounts.js
@@ -2,8 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 function SquareAccounts() {
   return (
-    <button type="button" className="btn btn-primary btn-lg col-6 p-0 ">
-      <Link to={'/CheckoutFlow'}>結帳</Link>
+    <button
+      type="button"
+      className="btn btn-primary btn-lg col-6 p-0 hover_style"
+    >
+      <Link to={'/CheckoutFlow'} className="link_color">
+        結帳
+      </Link>
     </button>
   )
 }
