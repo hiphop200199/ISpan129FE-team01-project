3f5c21706cd4c16b027540f1f2f50aebb92faa9c
Yun,Ning <dackdada016@gmail.com>
2023-03-03 10:39:21 +0800

    feat: 加入結帳共用元件，並移除多餘註解

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Link } from 'react-router-dom'
+import { SquareAccounts } from '../../template'
 
 const Cart = () => {
   // 取得購物車的資料並轉成Json
@@ -18,66 +17,52 @@ const Cart = () => {
 
   return (
     <div className="cart__sidebar">
-      <main className="cart__body">
-        <h2>購物車</h2>
-        <table>
-          <thead>
-            {/* <tr>
+      <main className="shopping__cart">
+        <div className="cart__body">
+          <h2>購物車</h2>
+          <table>
+            <thead>
+              {/* <tr>
               <th>名稱</th>
               <th>價格</th>
               <th>數量</th>
               <th>操作</th>
             </tr> */}
-          </thead>
-          <tbody>
-            {items.map(
-              (
-                {
-                  product_id,
-                  product_name,
-                  products_price,
-                  product_image,
-                  product_qry,
-                },
-                index
-              ) => (
-                <tr key={product_id}>
-                  <td>
-                    <img
-                      src={`http://localhost:3002/uploads/${product_image}`}
-                      alt={product_name}
-                    />
-                  </td>
-                  <td>{product_name}</td>
-                  <td>{products_price}</td>
-                  <td>{product_qry}</td>
-                  <td>
-                    <button onClick={() => removeItem(index)}>移除</button>
-                  </td>
-                </tr>
-              )
-            )}
-          </tbody>
-        </table>
-        {/* <form
-          onSubmit={(event) => {
-            event.preventDefault()
-            addItem({
-              name: event.target.name.value,
-              price: event.target.price.value,
-              quantity: event.target.quantity.value,
-            })
-            event.target.reset()
-          }}
-        >
-          <input type="text" name="name" placeholder="商品" />
-          <input type="number" name="price" placeholder="價格" />
-          <input type="number" name="quantity" placeholder="數量" />
-          <button type="submit">加入購物車</button>
-        </form> */}
-        <button>
-          <Link to={'/CheckoutFlow'}>結帳</Link>
-        </button>
+            </thead>
+            <tbody>
+              {items.map(
+                (
+                  {
+                    product_id,
+                    product_name,
+                    products_price,
+                    product_image,
+                    product_qry,
+                  },
+                  index
+                ) => (
+                  <tr key={product_id}>
+                    <td>
+                      <img
+                        src={`http://localhost:3002/uploads/${product_image}`}
+                        alt={product_name}
+                      />
+                    </td>
+                    <td>{product_name}</td>
+                    <td>{products_price}</td>
+                    <td>{product_qry}</td>
+                    <td>
+                      <button onClick={() => removeItem(index)}>移除</button>
+                    </td>
+                  </tr>
+                )
+              )}
+            </tbody>
+          </table>
+        </div>
+        <div className="aside d-flex justify-content-center">
+          <SquareAccounts className="more_color" />
+        </div>
       </main>
     </div>
   )
