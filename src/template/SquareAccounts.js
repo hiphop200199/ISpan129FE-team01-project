6c70158a9d1c7d5a94f0de4acbb822a4a372390b
Yun,Ning <dackdada016@gmail.com>
2023-03-03 10:41:58 +0800

    style: 調整樣式以符合RWD

--- src/template/SquareAccounts.js
+++ src/template/SquareAccounts.js
@@ -1,12 +1,9 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function SquareAccounts() {
   return (
-    <button
-      type="button"
-      className="btn btn-primary btn-lg min-width-auto col-2"
-    >
-      結帳
+    <button type="button" className="btn btn-primary btn-lg col-6 p-0 ">
+      <Link to={'/CheckoutFlow'}>結帳</Link>
     </button>
   )
 }
