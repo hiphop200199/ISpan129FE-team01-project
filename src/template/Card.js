11a8d333d83b35b0fa89d681323774f9b60dcbc8
Yun,Ning <dackdada016@gmail.com>
2023-03-03 10:41:42 +0800

    refactor: 修正資料欄位名稱以符合元件共用

--- src/template/Card.js
+++ src/template/Card.js
@@ -1,12 +1,9 @@
 import photo from '../img/productDetails/cheese.jpg'
 // { photo }帶入參數
 // function Card({photo}) {
+import { AddToCartLg, MoreSquare } from './index'
 
-import MoreSquare from './MoreSquare'
-import MoreRound from './MoreRound'
-
-function Card({ data, typeID }) {
-
+function Card({ product, typeID }) {
   const {
     product_id,
     product_name,
@@ -14,8 +11,8 @@ function Card({ data, typeID }) {
     products_price,
     products_descripttion,
     product_image,
-  } = data
-  // console.log(typeID)
+  } = product
+  console.log(product_image)
   return (
     <div className="productCard">
       <span hidden>{product_id}</span>
@@ -28,6 +25,7 @@ function Card({ data, typeID }) {
       <section className="buttons">
         <button className="button-collection">&#9825;</button>
         <MoreSquare product_id={product_id} typeID={typeID} />
+        <AddToCartLg product={product} />
         {/* <MoreSquare typeID={typeID} product_id={product_id} /> */}
       </section>
       {/* 圖片動態引入 ，圖片須放在public資料夾*/}
