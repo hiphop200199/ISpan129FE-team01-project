0926af2956a94451073f026d9f46ff80390e7580
Yun,Ning <107627148+dackdada016@users.noreply.github.com>
2023-03-05 17:37:48 +0800

    fix:修正共用SCSS的樣式衝突

--- src/template/Card.js
+++ src/template/Card.js
@@ -14,7 +14,7 @@ function Card({ product, typeID }) {
   } = product
   console.log(product_image)
   return (
-    <div className="productCard">
+    <div className="productCard col-4 m-auto">
       <span hidden>{product_id}</span>
       {/* <section className="text-part"> */}
       <h2 className="title">{product_name}</h2>
@@ -25,9 +25,9 @@ function Card({ product, typeID }) {
       <section className="buttons">
         <button className="button-collection">&#9825;</button>
         <MoreSquare product_id={product_id} typeID={typeID} />
-        <AddToCartLg product={product} />
         {/* <MoreSquare typeID={typeID} product_id={product_id} /> */}
       </section>
+      <AddToCartLg product={product} />
       {/* 圖片動態引入 ，圖片須放在public資料夾*/}
       <img src={`http://localhost:3002/${product_image}`} alt="" />
       {/* <img src={'http://localhost:3000/' + photo} alt="" /> */}
