bc5c4c87b1f8de7174d39276e2ed326bcffc6d02
4A555076 <fz555076@gmail.com>
2023-03-08 09:43:24 +0800

    feat : 新增活動步驟元件

---
+++ src/pages/activity/Stepper.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import 'react-step-progress-bar/styles.css'
+import { ProgressBar, Step } from 'react-step-progress-bar'
+
+function Stepper(props) {
+  return (
+    <>
+      <ProgressBar
+        percent={((props.step - 1) * 100) / 2}
+        filledBackground="linear-gradient(to right, #fefb72, #f0bb31)"
+      >
+        <Step transition="scale">
+          {({ accomplished, page }) => (
+            <div className={`step ${accomplished ? 'completed' : ''}`}>1</div>
+          )}
+        </Step>
+        <Step transition="scale">
+          {({ accomplished, page }) => (
+            <div className={`step ${accomplished ? 'completed' : ''}`}>2</div>
+          )}
+        </Step>
+        <Step transition="scale">
+          {({ accomplished, page }) => (
+            <div className={`step ${accomplished ? 'completed' : ''}`}>3</div>
+          )}
+        </Step>
+      </ProgressBar>
+    </>
+  )
+}
+
+export default Stepper
