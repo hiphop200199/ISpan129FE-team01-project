265c6ffe74a47333000a24dc9b7349fd990aa038
Eric Tsai <hiphop200199@gmail.com>
2023-03-08 11:42:34 +0800

    補上元件參數

--- src/pages/course/CourseDetail.js
+++ src/pages/course/CourseDetail.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 function CourseDetail() {
   const { product_id } = useParams()
-
+  const id = localStorage.getItem('id')
   const [courseDetail, setCourseDetail] = useState([])
   useEffect(() => {
     fetch(`http://localhost:3002/product/list-detail/${product_id}`)
@@ -13,6 +13,7 @@ function CourseDetail() {
       })
       .catch((error) => console.log(error))
   }, [product_id])
+
   return (
     <>
       {courseDetail.map((item, i) => {
@@ -32,8 +33,8 @@ function CourseDetail() {
               <div className="product-button-wrapper">
                 {/* <button className="product-add-collection">加入收藏</button>
               <button className="product-add-cart">加入購物車</button> */}
-                <AddToFavoritesLg />
-                <AddToCartLg />
+                <AddToFavoritesLg product={item} typeID={2} id={id} />
+                <AddToCartLg product={item} />
               </div>
             </div>
           </div>
