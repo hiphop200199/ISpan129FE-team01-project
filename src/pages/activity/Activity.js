1d3d7cc1f1586a44a078a88d754079a13f52920f
4A555076 <fz555076@gmail.com>
2023-03-14 02:53:00 +0800

    feat : 修改照片為多張以及新增報名按鈕可依時間判斷，顯示或隱藏

--- src/pages/activity/Activity.js
+++ src/pages/activity/Activity.js
@@ -1,93 +1,108 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { EventRegistration } from '../../template'
-import { Link, useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import SignUp from '../../template/SignUp'
-import Header from '../../layouts/header'
-import HeaderSearch from '../../layouts/HeaderSearch'
+import { EventRegistration } from '../../template'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-import 'swiper/css/navigation'
+import { Autoplay, Pagination, Navigation, FreeMode, EffectFade } from 'swiper'
+import cat from '../../img/activity/activityhome1.png'
+import dog from '../../img/activity/activityhome2.png'
+import twodog from '../../img/activity/activityhome3.png'
 
-import { Autoplay, Pagination, Navigation } from 'swiper'
 function Activity() {
   const [activity, setActivity] = useState([])
-  // const [ActivityPhoto, setActivityPhoto] = useState([])
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const res = await fetch(`http://localhost:3002/activity/api`, {
-  //       method: 'GET',
-  //     })
-  //     const avtivity = await res.json()
-  //     setActivity(activity)
-  //   }
-  //   fetchData()
-  // }, [])
   useEffect(() => {
-    fetch(`http://localhost:3002/activity/api`)
+    fetch(`http://localhost:3002/activity/activity-list/api`, {
+      method: 'GET',
+    })
       .then((response) => response.json())
-      .then((data) => {
-        console.log('data', data)
-        setActivity(data.rows)
+      .then((activity) => {
+        console.log(typeof activity)
+
+        const activities = [...activity]
+        activities.forEach((el) => {
+          console.log(el)
+          const activities = [...activity]
+          const img = el.activity_image.split(',')
+          const bigImage = img[0]
+          el.activity_img = bigImage
+          console.log('activities', activities)
+        })
+
+        // Sort activities by date
+        activities.sort((a, b) => {
+          const aDate = new Date(a.activity_dateend).getTime()
+          const bDate = new Date(b.activity_dateend).getTime()
+          return bDate - aDate
+        })
+
+        setActivity(activities)
       })
       .catch((error) => console.error(error))
   }, [])
+
+  const now = new Date().getTime()
+
   return (
     <>
-      <Header />
       <Swiper
         style={{
           '--swiper-navigation-color': 'white',
-          '--swiper-navigation-size': '20px',
+          '--swiper-navigation-size': '30px',
         }}
         centeredSlides={true}
+        effect={'fade'}
+        speed={2000}
         autoplay={{
-          delay: 3000,
+          delay: 5000,
           disableOnInteraction: false,
         }}
-        navigation={true}
         slidesPerView={1}
-        modules={[Autoplay, Pagination, Navigation]}
+        modules={[EffectFade, Autoplay, Pagination]}
         loop={true}
+        className="activity-home-swiper"
       >
-        {activity.map((photo, idx) => (
-          <SwiperSlide key={idx}>
-            <img
-              src={`http://localhost:3002/uploads/${photo.activity_image}/`}
-              alt=""
-              className="swiper-img"
-            />
-          </SwiperSlide>
-        ))}
+        <SwiperSlide>
+          <img src={cat} alt="" className="swiper-img" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={dog} alt="" className="swiper-img" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={twodog} alt="" className="swiper-img" />
+        </SwiperSlide>
       </Swiper>
-      <HeaderSearch />
-      search:
-      <input type="search" />
+      {/* <div>
+        search: <input type="search" />
+      </div> */}
       <div className="productContent col-12">
         {activity.map((el, idx) => {
+          const endDate = new Date(el.activity_dateend).getTime()
+          const expired = endDate < now
           return (
             <div className="productCard col-4 m-auto" key={idx}>
-              <p className="title">{el.activity_name}</p>
+              <h5 className="title">{el.activity_name}</h5>
               <p className="description">
                 活動日期 :
                 {new Date(el.activity_datestart).toString('yyyy-MM-dd')}
               </p>
               <p className="title">
-                截止日期 :{new Date(el.activity_dateend).toString('yyyy-MM-dd')}
+                截止日期:{new Date(el.activity_dateend).toString('yyyy-MM-dd')}
               </p>
               <section className="buttons">
-                <button className="button-collection">
-                  <Link to={`/ActivitySignUp/${el.activity_id}`}>
-                    <SignUp />
-                  </Link>
-                </button>
+                {!expired && (
+                  <button className="button-collection">
+                    <Link to={`/ActivitySignUp/${el.activity_id}`}>
+                      <SignUp />
+                    </Link>
+                  </button>
+                )}
                 <Link to={`/activitydetail/${el.activity_id}`}>
                   <EventRegistration />
                 </Link>
               </section>
               <img
-                src={`http://localhost:3002/uploads/${el.activity_image}/`}
+                src={`http://localhost:3002/uploads/${el.activity_img}`}
                 alt=""
               />
             </div>
