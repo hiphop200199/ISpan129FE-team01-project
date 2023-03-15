84665d7951ab6a8c67f3af18dfe9a4b191e5c511
Jin22221 <117150611+Jin22221@users.noreply.github.com>
2023-03-16 01:27:02 +0800

    feat : 在訂單總金額裡乘上入住幾晚數

--- src/pages/member/OrderDetail.js
+++ src/pages/member/OrderDetail.js
@@ -43,7 +43,12 @@ function OrderDetail() {
   const totalOrderPrice = (orderData) => {
     let orderTotal = 0
     orderData.forEach((item) => {
-      const itemTotal = item.product_quantity * item.product_price
+      console.log('=====item=====', JSON.parse(item.additional))
+      const additionalObj = JSON.parse(item.additional)
+      const itemTotal =
+        item.product_quantity *
+        item.product_price *
+        (item.additional ? additionalObj.differenceInDay : 1)
       orderTotal += itemTotal
     })
     return orderTotal
@@ -135,7 +140,9 @@ function OrderDetail() {
                     <td>{orderItem.product_price}</td>
                     <td>{orderItem.product_quantity}</td>
                     <td>
-                      {orderItem.product_quantity * orderItem.product_price}
+                      {orderItem.product_quantity *
+                        orderItem.product_price *
+                        (orderItem.additional ? additional.differenceInDay : 1)}
                     </td>
                   </tr>
                 ))}
