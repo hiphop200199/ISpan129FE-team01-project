974ab373ee1a27d03963ee6710aff80134839748
Chu-Ya Hsu <t199511137@gmail.com>
2023-03-20 16:02:41 +0800

    feat : 新增顯示密碼功能

--- src/pages/member/ChangePassword.js
+++ src/pages/member/ChangePassword.js
@@ -1,10 +1,22 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+import {
+  faCircleXmark,
+  faEye,
+  faEyeSlash,
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Swal from 'sweetalert2'
 
 function ChangePassword() {
+  const [showPassword, setShowPassword] = useState(false)
+  const [showPassword2, setShowPassword2] = useState(false)
+  const passwordType = showPassword ? 'text' : 'password'
+  const passwordType2 = showPassword2 ? 'text' : 'password'
+
+  const passwordIcon = showPassword ? faEye : faEyeSlash
+  const passwordIcon2 = showPassword2 ? faEye : faEyeSlash
+
   const navigate = useNavigate()
 
   const [password, setPassword] = useState('')
@@ -53,31 +65,59 @@ function ChangePassword() {
       .catch((error) => console.error(error))
   }
 
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword)
+  }
+
+  const handleShowPassword2 = () => {
+    setShowPassword2(!showPassword2)
+  }
+
   return (
     <>
       <section className="login-container">
         <div className="login-main">
           <form className="login-form" onSubmit={handleSubmit}>
             <h3 className="m-3">更新密碼</h3>
-            <input
-              className="form-control"
-              type="password"
-              value={password}
-              placeholder="新密碼:"
-              onChange={(event) => setPassword(event.target.value)}
-              minLength={6}
-              maxLength={10}
-            />
-            <br />
-            <input
-              className="form-control"
-              type="password"
-              value={password2}
-              placeholder="確認密碼:"
-              onChange={(event) => setPassword2(event.target.value)}
-              minLength={6}
-              maxLength={10}
-            />
+            <div className="typeArea">
+              <input
+                className="form-control"
+                type={passwordType}
+                value={password}
+                placeholder="新密碼:"
+                onChange={(event) => setPassword(event.target.value)}
+                minLength={6}
+                maxLength={10}
+                required
+              />
+              <FontAwesomeIcon
+                className="passwordGroup"
+                icon={passwordIcon}
+                onClick={handleShowPassword}
+                style={{
+                  position: 'absolute',
+                  top: '15%',
+                  transform: 'translateY(-23%)',
+                  color: '#515151',
+                }}
+              />
+              <br />
+              <input
+                className="form-control"
+                type={passwordType2}
+                value={password2}
+                placeholder="確認密碼:"
+                onChange={(event) => setPassword2(event.target.value)}
+                minLength={6}
+                maxLength={10}
+                required
+              />
+              <FontAwesomeIcon
+                className="passwordGroup"
+                icon={passwordIcon2}
+                onClick={handleShowPassword2}
+              />
+            </div>
             <br />
             <button className="btn btn-primary-for-login" type="submit">
               送出
